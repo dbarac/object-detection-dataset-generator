75799f5c30cb52a4503dd9ef2c88ce1b667ed2a9
dbarac <dario.barac1@gmail.com>
2020-09-02 20:15:07 +0200

    edit docstring, change class name and some method names

--- label_generator.py
+++ label_generator.py
@@ -10,7 +10,7 @@ import numpy as np
 #check if necessary environment variables are set
 env_vars = os.environ.copy()
 if "PYSOTPATH" not in env_vars:
-    print("Error: $PYSOTPATH needs to be set to find pysot trackers and config")
+    print("Error: $PYSOTPATH=/path/to/pysot needs to be set to find pysot trackers and config")
     exit(1)
 if "PYTHONPATH" not in env_vars:
     print("Error: /path/to/pysot needs to be in $PYTHONPATH find the pysot package")
@@ -22,15 +22,19 @@ from pysot.tracker.tracker_builder import build_tracker
 
 torch.set_num_threads(1)
 
-class LabelGenerator():
-    """Generate labels in COCO format for object detection"""
+class COCODatasetGenerator():
+    """Generates detection annotations in COCO format by tracking objects in videos."""
 
-    def __init__(self, dataset_name, bounding_box_init_method, image_display_method):
-        self.get_bounding_box = bounding_box_init_method
-        self.display_image = image_display_method
+    def __init__(self, dataset_name, tracker_config, tracker_snapshot):
+        """
+        Args:
+          - dataset_name (str): Where all images and annotations will be saved
+          - tracker_config (str): Path to pysot config file for the tracker
+          - tracker_snapshot (str): Path to .pth file of pysot tracker
+        """
         self.dataset_name = dataset_name
         self.saved_frames = set()
-
+        self.load_tracker(tracker_config, tracker_snapshot)
         #create dataset directory if it doesn't exist
         if not os.path.isdir("results"):        
             os.mkdir("results")
@@ -45,19 +49,23 @@ class LabelGenerator():
             self.dataset = self.initialize_dataset()
 
 
-    @staticmethod
-    def load_tracker(tracker_config, tracker_snapshot):
+    def load_tracker(self, tracker_config, tracker_snapshot):
+        """Load the selected pysot tracker.
+
+        Args:
+          - tracker_config (str): Path to pysot config file for the tracker
+          - tracker_snapshot (str): Path to .pth file of pysot tracker
+        """
         cfg.merge_from_file(tracker_config)
         cfg.CUDA = torch.cuda.is_available() and cfg.CUDA
         device = torch.device('cuda' if cfg.CUDA else 'cpu')
         model = ModelBuilder()
-        model.load_state_dict(torch.load(tracker_snapshot,
-                                         map_location=lambda storage, loc: storage.cpu()))
+        model.load_state_dict(torch.load(tracker_snapshot))
         model.eval().to(device)
-        LabelGenerator.tracker = build_tracker(model)
+        self.tracker = build_tracker(model)
 
 
-    def generate_multi_object_labels(self, video_path, class_name):
+    def annotate_multi_object_video(self, video_path, class_name):
         """
         Make bounding box annotations for video with multiple objects
         of the same class.
@@ -65,22 +73,26 @@ class LabelGenerator():
         After selecting the number of objects, the tracker will
         run once for each of the objects.
         (pysot trackers do not support multi object tracking)
+
+        Args:
+          - video_path (str)
+          - class_name (str)
         """
         video = cv2.VideoCapture(video_path)
         retval, first_frame = video.read()
         first_frame = imutils.resize(first_frame, height=800)
         del video
         print("How many objects to track?")
-        cv2.imshow("COCO label generator", first_frame)
+        cv2.imshow("COCO dataset generator", first_frame)
         cv2.waitKey(2)
         n_objects = int(input())
         for i in range(n_objects):
             print("Tracking object", i+1)
-            self.generate_labels(video_path, class_name, remember_saved_frames=True)
+            self.annotate_video_frames(video_path, class_name, remember_saved_frames=True)
 
 
-    def generate_labels(self, video_path, class_name,
-                        discard_blurry_frames=False, remember_saved_frames=False):
+    def annotate_video_frames(self, video_path, class_name,
+                              discard_blurry_frames=False, remember_saved_frames=False):
         """
         Add bounding box annotations to the dataset for a given video.
 
@@ -91,6 +103,13 @@ class LabelGenerator():
         Tracking can be paused by pressing 'p'.
         By pressing 'r' the user can reset the tracker by selecting the bbox
         for the current frame.
+        By pressing 'n' the user can end the tracking process for the video.
+
+        Args:
+          - video_path (str)
+          - class_name (str)
+          - discard_blurry_frames (bool): If true, blurry video frames won't be saved
+          - remember_saved_frames (bool): Used when running multiple times on the same video
         """
         video = cv2.VideoCapture(video_path)
         category_id = self.add_category(class_name)
@@ -117,7 +136,7 @@ class LabelGenerator():
                 
                 should_be_saved = False
                 if discard_blurry_frames:
-                    blurry = self.bbox_is_blurry(frame, x, y, w, h)
+                    blurry = self.bbox_is_blurry(frame, [x, y, w, h])
                     if not blurry:
                         valid_count += 1
                         if valid_count % frames_to_skip == 0:
@@ -154,13 +173,18 @@ class LabelGenerator():
         print("{} images have been added to '{}' dataset".format(saved_frames, self.dataset_name))
 
 
-    def bbox_is_blurry(self, frame, x, y, w, h):
+    def bbox_is_blurry(self, frame, bbox):
         """
         Check amount of blur inside bbox area.
 
         High variance of laplacian value ==> lots of edges <==> low blur
         Threshold should be set depending on object type.
+
+        Args:
+          - frame (numpy.ndarray)
+          - bbox (List[int])
         """
+        x, y, w, h = bbox
         roi = frame[y:y+h, x:x+w].copy()
         roi_gray = cv2.cvtColor(roi, cv2.COLOR_RGB2GRAY)
         roi_normalized = cv2.resize(roi_gray, dsize=(64,64))
@@ -174,15 +198,19 @@ class LabelGenerator():
             return False
 
 
-    def label_image_manually(self, image_path, class_name):
+    def annotate_image_manually(self, image_path, class_name):
         """
-        Select a bounding box for given image and add the
+        Select bounding boxes for given image and add the
         annotation and image to the dataset.
+
+        Args:
+          - image_path (str)
+          - class_name (str)
         """
         image = cv2.imread(image_path)
         image = imutils.resize(image, height=800)
         height, width = image.shape[0], image.shape[1]
-        cv2.imshow("COCO label generator", image)
+        cv2.imshow("COCO dataset generator", image)
         cv2.waitKey(5)
         n_boxes = input("How many bounding boxes? (default = 1):") or 1
         n_boxes = int(n_boxes)
@@ -201,9 +229,7 @@ class LabelGenerator():
 
 
     def initialize_dataset(self):
-        """
-        Create basic structure for a COCO detection dataset json file.
-        """
+        """Create basic structure for a COCO detection dataset json file."""
         return {
             "images": [],
             "categories": [],
@@ -215,6 +241,9 @@ class LabelGenerator():
         """
         Add object category to dataset json if it doesn't exist
         and return id of the category.
+
+        Args:
+          - category_name (str)
         """
         max_id = 0
         for cat in self.dataset["categories"]:
@@ -232,8 +261,11 @@ class LabelGenerator():
 
 
     def save_image(self, image, image_name):
-        """
-        Save a a single image to results/dataset_name/.
+        """Save a a single image to results/dataset_name/.
+
+        Args:
+          - image (numpy.ndarray)
+          - image_name (str)
         """
         if not os.path.isdir("results"):        
             os.mkdir("results")
@@ -246,6 +278,10 @@ class LabelGenerator():
         """
         Save image info to json COCO dataset.
         Return image id.
+
+        Args:
+          - image (numpy.ndarray)
+          - image_name (str)
         """
         if image_name in self.saved_frames:
             for image in self.dataset["images"]:
@@ -268,8 +304,12 @@ class LabelGenerator():
 
 
     def save_bbox_info(self, image_id, bboxes, category_id):
-        """
-        Save all image bounding boxes to json COCO dataset.
+        """Save all image bounding boxes to json COCO dataset.
+
+        Args:
+          - image_id (int)
+          - bboxes (List[List[int]])
+          - category_id (int)
         """
         annotation_id = 0
         if len(self.dataset["annotations"]) > 0:
@@ -295,41 +335,47 @@ class LabelGenerator():
         print(" - {} annotations".format(len(self.dataset["annotations"])))
 
 
-def get_bounding_box(image):
-    """
-    Ask user to select ROI.
-    If the selection was incorrect/canceled (c is pressed), ask for new selection.
-    """
-    redo_selection = (0,0,0,0)
-    while True:
-        cv2.imshow("COCO label generator", image)
-        selection = cv2.selectROI("COCO label generator", image)
-        if selection != redo_selection:
-            break
-    x, y, w, h = selection
-    #print(selection)
-    return (x, y, w, h)
+    def get_bounding_box(self, image):
+        """
+        Ask user to select ROI.
+        If the selection was incorrect/canceled (c is pressed), ask for new selection.
+
+        Args:
+          - image (numpy.ndarray)
+        """
+        redo_selection = (0,0,0,0)
+        while True:
+            cv2.imshow("COCO dataset generator", image)
+            selection = cv2.selectROI("COCO dataset generator", image)
+            if selection != redo_selection:
+                break
+        x, y, w, h = selection
+        return (x, y, w, h)
 
 
-def display_frame(frame, outputs):
-    """display frame and draw a bounding box"""
-    #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 1)
-    if 'polygon' in outputs:
-        mask = ((outputs['mask'] > cfg.TRACK.MASK_THERSHOLD) * 255)
-        mask = mask.astype(np.uint8)
-        mask = np.stack([mask, mask*255, mask]).transpose(1, 2, 0)
-        frame = cv2.addWeighted(frame, 0.77, mask, 0.23, -1)
-        bbox = list(map(int, outputs['bbox']))
-        cv2.rectangle(frame, (bbox[0], bbox[1]),
-                      (bbox[0]+bbox[2], bbox[1]+bbox[3]),
-                      (0, 255, 0), 1)
-    else:
-        bbox = list(map(int, outputs['bbox']))
-        cv2.rectangle(frame, (bbox[0], bbox[1]),
-                      (bbox[0]+bbox[2], bbox[1]+bbox[3]),
-                      (0, 255, 0), 1)
-    cv2.imshow("COCO label generator", frame)
-    return cv2.waitKey(1) #return key pressed by user
+    def display_image(self, image, outputs):
+        """Draw outputs (masks, bboxes) and display image.
+
+        Args:
+          - image (numpy.ndarray)
+          - outputs (Dict)
+        """
+        if 'polygon' in outputs:
+            mask = ((outputs['mask'] > cfg.TRACK.MASK_THERSHOLD) * 255)
+            mask = mask.astype(np.uint8)
+            mask = np.stack([mask, mask*255, mask]).transpose(1, 2, 0)
+            image = cv2.addWeighted(image, 0.77, mask, 0.23, -1)
+            bbox = list(map(int, outputs['bbox']))
+            cv2.rectangle(image, (bbox[0], bbox[1]),
+                          (bbox[0]+bbox[2], bbox[1]+bbox[3]),
+                          (0, 255, 0), 1)
+        else:
+            bbox = list(map(int, outputs['bbox']))
+            cv2.rectangle(image, (bbox[0], bbox[1]),
+                          (bbox[0]+bbox[2], bbox[1]+bbox[3]),
+                          (0, 255, 0), 1)
+        cv2.imshow("COCO dataset generator", image)
+        return cv2.waitKey(1) #return key pressed by user
 
 
 def main():
@@ -366,13 +412,11 @@ def main():
     print(" - discard blurry frames:", args["discard_blurry_frames"])
     print(" - tracker config:", args["tracker_config"])
     print(" - tracker model snapshot:", args["tracker_snapshot"])
-    
-    LabelGenerator.load_tracker(args["tracker_config"], args["tracker_snapshot"])
-    label_generator = LabelGenerator(args["dataset"],
-                                     bounding_box_init_method=get_bounding_box,
-                                     image_display_method=display_frame)
 
-    #run depending on configuration
+    dataset_generator = COCODatasetGenerator(args["dataset"],
+                                             args["tracker_config"], args["tracker_snapshot"])
+
+    #run with given configuration
     if args["dir"]:
         files = [args["dir"] + filename for filename in os.listdir(args["dir"])]
         if args["validation"]:
@@ -380,26 +424,26 @@ def main():
                 if not os.path.isfile(image_file):
                     continue
                 print(image_file)
-                label_generator.label_image_manually(image_file, args["class"])
+                dataset_generator.annotate_image_manually(image_file, args["class"])
                 print("{}/{} images saved.".format(i+1, len(files)))
             print("All images in {} have been labeled.".format(args["dir"]))
         else:
             for i, video_file in enumerate(files):
                 if args["multi_object"]:
-                    label_generator.generate_multi_object_labels(video_file, args["class"])
+                    dataset_generator.annotate_multi_object_video(video_file, args["class"])
                 else:
-                    label_generator.generate_labels(video_file,  args["class"],
-                                                    args["discard_blurry_frames"])
+                    dataset_generator.annotate_video_frames(video_file,  args["class"],
+                                                            args["discard_blurry_frames"])
                 print("{}/{} videos processed".format(i+1, len(files)))
             print("Labels have been generated for all videos in {}".format(args["dir"]))
     else:
         video_file = args["video"]
         if args["multi_object"]:
-            label_generator.generate_multi_object_labels(video_file, args["class"])
+            dataset_generator.annotate_multi_object_video(video_file, args["class"])
         else:
-            label_generator.generate_labels(video_file,  args["class"],
-                                            args["discard_blurry_frames"])
-    label_generator.print_dataset_stats()
+            dataset_generator.annotate_video_frames(video_file,  args["class"],
+                                                    args["discard_blurry_frames"])
+    dataset_generator.print_dataset_stats()
 
 
 if __name__ == '__main__':
